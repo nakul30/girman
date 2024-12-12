9acbfca7a93698024f87cdace987c4917f0ffddf
Nakul <nakulgarg1424@gmail.com>
2024-12-13 00:56:17 +0530

    Navbar Responsive

--- frontend/src/components/Home/Navbar.tsx
+++ frontend/src/components/Home/Navbar.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import logoImage from "../../assets/logo.png";
 import { IoIosMenu } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 const Navbar: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -12,25 +13,25 @@ const Navbar: React.FC = () => {
   return (
     <nav className="bg-white shadow-md h-[8vh] flex justify-center items-center">
       <div className="flex justify-around items-center w-[100%] md:h-[80%] md:w-[80%] ">
-        <div className="h-[60%] w-[20%] md:h-[80%] md:w-[14%] lg:h-[100%] lg:w-[13%]">
+        <Link to={"/"} className="h-[60%] w-[20%] md:h-[80%] md:w-[14%] lg:h-[100%] lg:w-[13%]">
           <img
             className="h-full w-full"
             src={logoImage}
             alt="Girman Technologies"
           />
-        </div>
+        </Link>
 
         <div className="md:hidden cursor-pointer z-20" onClick={toggleMenu}>
           <IoIosMenu className="h-[100%] w-[130%]" />
         </div>
 
         <div className="hidden md:flex text-[80%] items-center justify-center lg:text-[100%]">
-          <a
-            href="#"
+          <Link
+            to={"/"}
             className="black hover:text-blue-600 hover:underline mx-[5%]"
           >
             SEARCH
-          </a>
+          </Link>
           <a
             href="https://girmantech.com/"
             className="black hover:text-gray-900 hover:underline mx-[5%]"
@@ -54,13 +55,13 @@ const Navbar: React.FC = () => {
         {isMenuOpen && (
           <div className="md:hidden bg-white shadow-lg rounded-md w-[20%] top-[8vh] right-[15%] absolute text-[70%]">
             <div className="flex flex-col items-center py-4">
-              <a
-                href="#"
+              <Link
+                to={"/"}
                 className="block py-2 hover:text-blue-600 hover:underline"
                 onClick={toggleMenu}
               >
                 SEARCH
-              </a>
+              </Link>
               <a
                 href="https://girmantech.com/"
                 className="block py-2 hover:text-gray-900 hover:underline"
