b952340e8c938bb54ddfe6ad3f907b63fd057de2
Nakul <nakulgarg1424@gmail.com>
2024-12-12 20:34:50 +0530

    search setting

--- frontend/src/components/Home/Search.tsx
+++ frontend/src/components/Home/Search.tsx
@@ -3,13 +3,12 @@ import search_logo from "../../assets/search_logo.svg";
 import { FaSearch } from "react-icons/fa";
 const Search: React.FC = () => {
   return (
-    <div className="bg-custom-gradient flex flex-col justify-center items-center  h-[60vh]">
-      <div className="mb-[2%]  flex justify-center w-[40%]">
+    <div className="bg-custom-gradient flex flex-col justify-start items-center  h-screen w-screen mt-[1%]">
+      <div className="mb-[2%] mt-[8%] flex justify-center w-[40%] items-center">
         <img src={search_logo} alt="Search_Logo" className="w-full" />
       </div>
       <div className="flex items-center border border-gray-300 rounded-lg p-2 w-[40%] bg-white">
         <FaSearch className="text-gray-500" />
-
         <input
           type="text"
           placeholder="Search"
